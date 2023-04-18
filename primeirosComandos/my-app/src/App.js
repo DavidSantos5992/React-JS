7950c5a1c40ef87cdba37b99076b87326ee90257
David Henrique Silva dos Santos <davidhenriquehy@gmail.com>
2023-04-18 20:36:12 -0300

    components

---
+++ primeirosComandos/my-app/src/App.js
@@ -0,0 +1,14 @@
+import Nome from './components/Nome';
+
+function App() {
+  return (
+    <div>
+      <h1>Bem vindo</h1>
+      <Nome aluno="David" idade={23} />
+    </div>
+  );
+}
+
+export default App;
+
+
---
+++ primeirosComandos/my-app/src/components/Nome.js
@@ -0,0 +1,7 @@
+function Nome({aluno, idade}) {/* desestruturação de codigo */
+    return(
+      <p>Ola: {aluno} idade - {idade} anos </p>
+    );
+  }
+
+  export default Nome;
